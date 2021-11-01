b0743cb728456cd4df321b28f3cf65f4f7e55e3f
George Ciubotaru <geeku96@gmail.com>
2021-11-01 11:35:11 +0200

    feat: grid layout config

---
+++ src/components/common-styles/Grid.stories.svelte
@@ -0,0 +1,131 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import Grid from "./Grid.svelte";
+</script>
+
+<Meta title="Example/Grid" component={Grid} />
+
+<Template let:args>
+    <div style="background-color: cadetblue">
+        <div class="container">
+            <div class="row">
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+
+                <div
+                    class="col xs-1"
+                    style="height: 100px; background-color: blue"
+                >
+                    <div
+                        class="wrapper"
+                        style="height: 100%; width: 100%; background-color: green"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</Template>
+
+<Story name="Default" />
