ddfc1c2fc77c847e0b920262426cb7430f943562
George Ciubotaru <geeku96@gmail.com>
2022-01-05 17:15:27 +0200

    feat: resolve imports

--- src/components/WalletProvider/index.svelte
+++ src/components/WalletProvider/index.svelte
@@ -2,7 +2,7 @@
     import Web3 from "web3";
     import { setContext, onMount } from "svelte";
     import { writable } from "svelte/store";
-    import { isMobile } from "@walletconnect/browser-utils/dist/cjs";
+    import { isMobile } from "@walletconnect/browser-utils/dist/cjs/browser";
     import { signatureMessage, STATUS, errorCodes } from "./utils";
 
     export let chainId: string;
